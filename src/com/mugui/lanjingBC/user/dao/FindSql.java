0ad53667faa7bffbc4dcfc260f0b224b8626d1bf
e <1274983485@qq.com>
2019-05-21 14:36:01 +0800

    首次配置

---
+++ src/com/mugui/lanjingBC/user/dao/FindSql.java
@@ -0,0 +1,10 @@
+package com.mugui.lanjingBC.user.dao;
+
+import org.springframework.stereotype.Component;
+
+import com.mugui.DB.SqlModel;
+
+@Component
+public class FindSql extends SqlModel {
+
+}
